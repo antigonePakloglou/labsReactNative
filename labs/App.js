52a1f7ac2190f9193908ff893edd74199efddde3
antigonePakloglou <antigone1612@hotmail.fr>
2023-08-29 10:34:11 +0200

    lab02 section list

--- labs/App.js
+++ labs/App.js
@@ -1,16 +1,59 @@
 
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, SectionList } from 'react-native';
 
 
 export default function App() {
+  const students = [
+    {
+      title: 'Ingé 1',
+      data: [
+        "Aline", 
+        'Jack', 
+        'Antigone'
+      ]
+    },
+    {
+      title: 'Ingé 2',
+      data: [
+        'Martin',
+        'Michel',
+        'Cindy',
+        'Cédric',
+      ]
+    },
+    {
+      title: 'Ingé 2',
+      data: [
+        'Anna',
+        'Léon',
+        'Omar',
+        'Corine',
+      ]
+    }
+  ];
 
 
   return (
     <View style={styles.container}>
-      <Text>Image depuis le télephone</Text>
-      <Image style={styles.img} source={require('./assets/images/img1.jpg')} />
-      <Text>Image depuis internet</Text>
-      <Image style={styles.img} source={{uri: "https://th.bing.com/th/id/R.0e4a004c119aa6af10ef198adfbd0d0e?rik=b%2b76nu5Ou%2bcw6A&pid=ImgRaw&r=0"}} />
+
+      <SectionList sections={students} 
+      renderItem={({item})=> 
+        <View style={styles.studentCard}>
+          <Text style={styles.studentName}>{item}</Text>
+        </View> 
+      }
+      keyExtractor={(item, index) => index}
+      renderSectionHeader={ ({section}) =>  (
+        <View style={styles.sectionHeader}>
+          <Text style={styles.headerTitle}>{section.title}</Text>
+        </View>
+      )}
+      renderSectionFooter={({section}) => (
+        <View style={styles.sectionFooter}> 
+          <Text style={styles.footerText}>Total {section.data.length}</Text>
+        </View>
+      )}
+      />
     </View>
   );
 }
@@ -18,17 +61,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //vertical
+  },
+  studentCard: {
+    backgroundColor: 'grey',
+    width: '100%',
+    paddingHorizontal: 5,
+    paddingVertical: 20,
+    marginVertical: 5,
+  },
+  studentName :{
+    color: 'pink',
+    fontSize: 20
+  },
+  sectionHeader:{
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    backgroundColor: 'pink',
+    fontSize: 20,
     justifyContent: 'center',
-    //horizontal
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  img: {
-    width: 300,
-    height: 300,
-    resizeMode: 'contain',
-    backgroundColor: 'red'
+  headerTitle : {
+    color: 'black',
+    fontWeight: 'bold'
   },
-
-
+  sectionFooter:{
+    paddingVertical: 10,
+    alignItems: 'flex-end',
+    paddingHorizontal: 10
+  },
+  footerText : {
+    fontSize: 16
+  }
+ 
 });
