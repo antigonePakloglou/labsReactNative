1676218a7a39ffd0f686853bfad8efa577df1541
antigonePakloglou <antigone1612@hotmail.fr>
2023-08-29 10:57:32 +0200

    lab02galeriephoto

--- labs/App.js
+++ labs/App.js
@@ -1,59 +1,37 @@
 
-import { StyleSheet, View, Text, SectionList } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Image, FlatList, Dimensions } from 'react-native';
 
+const WINDOW = Dimensions.get("window");
+const IMG_URL = "https://picsum.photos/id/@id/200";
 
 export default function App() {
-  const students = [
-    {
-      title: 'Ingé 1',
-      data: [
-        "Aline", 
-        'Jack', 
-        'Antigone'
-      ]
-    },
-    {
-      title: 'Ingé 2',
-      data: [
-        'Martin',
-        'Michel',
-        'Cindy',
-        'Cédric',
-      ]
-    },
-    {
-      title: 'Ingé 2',
-      data: [
-        'Anna',
-        'Léon',
-        'Omar',
-        'Corine',
-      ]
-    }
-  ];
 
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+  let photoTmp = [];
+  //crée url pour un nbr d'image définie
+  for(var i=0; i < 50; i++){
+    photoTmp.push({
+      id: i, 
+      url: IMG_URL.replace("@id", i)
+    });
+  }
+   //mise a jour de la variable
+   setPhotos(photoTmp);
+  }, []);
 
   return (
     <View style={styles.container}>
-
-      <SectionList sections={students} 
-      renderItem={({item})=> 
-        <View style={styles.studentCard}>
-          <Text style={styles.studentName}>{item}</Text>
-        </View> 
-      }
-      keyExtractor={(item, index) => index}
-      renderSectionHeader={ ({section}) =>  (
-        <View style={styles.sectionHeader}>
-          <Text style={styles.headerTitle}>{section.title}</Text>
-        </View>
-      )}
-      renderSectionFooter={({section}) => (
-        <View style={styles.sectionFooter}> 
-          <Text style={styles.footerText}>Total {section.data.length}</Text>
-        </View>
-      )}
+      <FlatList 
+        data={photos}
+        renderItem={({item}) => (
+          <Image style={styles.image}  source={{uri: item.url}}/>
+        )}
+        numColumns={3}
       />
+   
     </View>
   );
 }
@@ -61,37 +39,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  studentCard: {
-    backgroundColor: 'grey',
-    width: '100%',
-    paddingHorizontal: 5,
-    paddingVertical: 20,
-    marginVertical: 5,
-  },
-  studentName :{
-    color: 'pink',
-    fontSize: 20
-  },
-  sectionHeader:{
-    paddingHorizontal: 10,
-    paddingVertical: 20,
-    backgroundColor: 'pink',
-    fontSize: 20,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
-  headerTitle : {
-    color: 'black',
-    fontWeight: 'bold'
-  },
-  sectionFooter:{
-    paddingVertical: 10,
-    alignItems: 'flex-end',
-    paddingHorizontal: 10
-  },
-  footerText : {
-    fontSize: 16
+  image : {
+    height: 200,
+    width: WINDOW.width / 3
   }
  
 });
